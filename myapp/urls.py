0bd4255153228330b393e4a37a60256372fd77db
MuhdaliN <muhammedalinoushad7@gmail.com>
2026-02-06 11:55:48 +0530

    modified

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-				path('home/',views.home,name='home'),
+				path('',views.home,name='home'),
 				path('about/',views.about,name='about'),
 				path('contact/',views.contact,name='contact'),
 				path('register/',views.register,name='register' ),
